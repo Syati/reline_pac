efaf566d5f4e13042f6c02bc02c7a077779328c9
mizuki-y <mizuki-y@syati.info>
2026-01-17 13:56:48 +0900

    initial commit

---
+++ spec/reline_pac_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe RelinePac do
+  it "has a version number" do
+    expect(RelinePac::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
